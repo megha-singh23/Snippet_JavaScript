66113f28a2212f6c7da6c20014534b8a3d6afefd
Megha Singh <meghashakya235@gmail.com>
2025-03-18 21:55:34 -0500

    minor changes

---
+++ react/helloreact/src/Components/Navbar.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav class="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">E-Commerce</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">About</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Services
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><hr class="dropdown-divider"/></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">ContactUS</a>
+        </li>
+      </ul>
+      <div className='grid gap-0 column-gap-3'>
+      <button className="btn btn-primary">Login</button>
+      <button className='btn btn-success'>Register</button>
+      </div>
+    </div>
+  </div>
+</nav>
+  )
+}
+
+export default Navbar;
